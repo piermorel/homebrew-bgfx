271e3fe87a52f76b1ffba6881eddb18fd13fafd6
Pierre Morel <pier.morel@gmail.com>
2023-08-31 16:36:14 +0200

    added cmake build of bgfx

---
+++ bgfx.cmake.rb
@@ -0,0 +1,39 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class BgfxCmake < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/bkaradzic/bgfx.cmake.git"
+  version "1"
+  sha256 ""
+  license ""
+
+  depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    #system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "git submodule init"
+    system "git submodule update"
+    
+     system "cmake", "-S.", "-Bcmake-build", *std_cmake_args
+     system "cmake", "--build","cmake-build"
+     system "cmake", "--install","cmake-build"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test bgfx.cmake`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
